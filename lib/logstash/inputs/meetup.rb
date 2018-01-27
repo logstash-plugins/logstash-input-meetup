6a4c8c9647eca0a647175723695bbcec036cf7b5
Jake Landis <jake.landis@elastic.co>
2018-01-26 18:18:03 -0600

    Minor: reformat meetup.rb
    Fixes #12

--- lib/logstash/inputs/meetup.rb
+++ lib/logstash/inputs/meetup.rb
@@ -35,21 +35,21 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
     require "faraday"
     # group_id
     if groupid
-	addon = "group_id=#{ @groupid }"
-    # group_urlname
+      addon = "group_id=#{ @groupid }"
+      # group_urlname
     elsif urlname
-	addon = "group_urlname=#{ @urlname }"
-    # venue_id
+      addon = "group_urlname=#{ @urlname }"
+      # venue_id
     elsif venueid
-	addon = "venue_id=#{ @venueid }"
+      addon = "venue_id=#{ @venueid }"
     else
-    # None Selected, raise an error
-    raise "Configuration error! -  Must have one of `urlname`, `venue_id`, or `group_id` defined"
-	addon = ""
+      # None Selected, raise an error
+      raise "Configuration error! -  Must have one of `urlname`, `venue_id`, or `group_id` defined"
+      addon = ""
     end
     @url = "https://api.meetup.com/2/events.json?key=#{ @meetupkey.value }&status=#{ @eventstatus }&#{ addon }"
     @logger.info("Registering meetup Input", :url => @url.gsub(@meetupkey.value, "xxxx"), :interval => @interval)
-  end # def register
+  end  # def register
 
   public
   def run(queue)
@@ -75,7 +75,7 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
         event.set('[group][created]', LogStash::Timestamp.at(event.get('group][created]') / 1000, (event.get('group][created]') % 1000) * 1000))
         event.set('updated', LogStash::Timestamp.at(event.get('updated') / 1000, (event.get('updated') % 1000) * 1000))
         event.set('[venue][lonlat]', [event.get('[venue][lon]'), event.get('[venue][lat]')]) if rawevent.has_key?('venue')
-        event.set('[group][lonlat]', [event.get('[group][group_lon]'),event.get('[group][group_lat]')]) if rawevent.has_key?('group')
+        event.set('[group][lonlat]', [event.get('[group][group_lon]'), event.get('[group][group_lat]')]) if rawevent.has_key?('group')
         decorate(event)
         queue << event
       end

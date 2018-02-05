a1b2df36c3407340c427dbd47b6b4d77a4551321
siobahnv <siobahnv@gmail.com>
2018-02-04 22:35:43 -0800

    Added text search config option
    Fixes #13

--- lib/logstash/inputs/meetup.rb
+++ lib/logstash/inputs/meetup.rb
@@ -10,16 +10,20 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
   config_name "meetup"
 
   # URLName - the URL name ie `ElasticSearch-Oklahoma-City`
-  # Must have one of urlname, venue_id, group_id
+  # Must have one of urlname, venue_id, group_id, or text
   config :urlname, :validate => :string
 
   # The venue ID
-  # Must have one of `urlname`, `venue_id`, `group_id`
+  # Must have one of `urlname`, `venue_id`, `group_id`, or 'text'
   config :venueid, :validate => :string
 
   # The Group ID, multiple may be specified seperated by commas
-  # Must have one of `urlname`, `venueid`, `groupid`
+  # Must have one of `urlname`, `venueid`, `groupid`, or 'text'
   config :groupid, :validate => :string
+
+  # A text string, AND_text is set to true
+  # Must have one of urlname, venue_id, group_id, or text
+  config :text, :validate => :string
 
   # Interval to run the command. Value is in minutes.
   config :interval, :validate => :number, :required => true
@@ -33,21 +37,30 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
   public
   def register
     require "faraday"
-    # group_id
-    if groupid
-      addon = "group_id=#{ @groupid }"
-      # group_urlname
-    elsif urlname
-      addon = "group_urlname=#{ @urlname }"
-      # venue_id
-    elsif venueid
-      addon = "venue_id=#{ @venueid }"
+
+    if text
+      # do this
+        addon = "text=#{ @text }&and_text=true"
+        @url = "https://api.meetup.com/2/open_events.json?key=#{ @meetupkey }&#{ addon }"
     else
-      # None Selected, raise an error
-      raise "Configuration error! -  Must have one of `urlname`, `venue_id`, or `group_id` defined"
-      addon = ""
+        # group_id
+      if groupid
+        addon = "group_id=#{ @groupid }"
+        # group_urlname
+      elsif urlname
+        addon = "group_urlname=#{ @urlname }"
+        # venue_id
+      elsif venueid
+        addon = "venue_id=#{ @venueid }"
+      else
+        # None Selected, raise an error
+        raise "Configuration error! -  Must have one of `urlname`, `venue_id`, or `group_id` defined"
+        addon = ""
+      end
+
+      @url = "https://api.meetup.com/2/events.json?key=#{ @meetupkey.value }&status=#{ @eventstatus }&#{ addon }"
     end
-    @url = "https://api.meetup.com/2/events.json?key=#{ @meetupkey.value }&status=#{ @eventstatus }&#{ addon }"
+
     @logger.info("Registering meetup Input", :url => @url.gsub(@meetupkey.value, "xxxx"), :interval => @interval)
   end  # def register
 
@@ -68,6 +81,10 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
       end
 
       result["results"].each do |rawevent|
+        # handling MU's "relevant" but not matching results
+        next if rawevent['description'].nil? 
+        next unless rawevent['description'].downcase.include? @text.downcase
+
         event = LogStash::Event.new(rawevent)
         # Convert the timestamps into Ruby times
         event.set('created', LogStash::Timestamp.at(event.get('created') / 1000, (event.get('created') % 1000) * 1000))
@@ -76,6 +93,7 @@ class LogStash::Inputs::Meetup < LogStash::Inputs::Base
         event.set('updated', LogStash::Timestamp.at(event.get('updated') / 1000, (event.get('updated') % 1000) * 1000))
         event.set('[venue][lonlat]', [event.get('[venue][lon]'), event.get('[venue][lat]')]) if rawevent.has_key?('venue')
         event.set('[group][lonlat]', [event.get('[group][group_lon]'), event.get('[group][group_lat]')]) if rawevent.has_key?('group')
+        event.set('[rating][average]', event.get('[rating][average]').round) if rawevent.has_key?('rating')       
         decorate(event)
         queue << event
       end
